8089084dc985605555361aca07bef734ec9d79d5
eeliya nayeri <eeliya.nayeri@gmail.com>
2025-03-28 17:52:41 +0000

    Replaced Labels with placeholder text, Input fields highlight red if caught by error handle, active filter added for active nav bar item + general fixes

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,16 @@ from django import forms
 from .models import CustomUser, Employee, Team
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    team = forms.ModelChoiceField(queryset=Team.objects.all())
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    team = forms.ModelChoiceField(
+    queryset=Team.objects.all(),
+    empty_label="Team",
+    widget=forms.Select(attrs={'placeholder': 'Team'})
+)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), help_text=UserCreationForm.base_fields['password1'].help_text)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+    first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
 
     class Meta:
         model = CustomUser
@@ -20,4 +26,8 @@ class RegisterForm(UserCreationForm):
             'password2',
             'team',
         )
-    
+
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+                    }
+       
