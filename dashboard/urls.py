f0ff6598e9bd5bd95a8cb7c1dcd8c2bb928236ec
Moonsa1245 <musa.hoque@outlook.com>
2025-04-08 14:58:08 +0100

    Changes made: - Added 2 new files: Account.html, Account.css - Changes to dashboard/url.py: added a path for the Account.html page for navigation. - Changes to dashboard/views.py: which checks for requests made for navigation to the Account.html page. - Account.html contains: All basic layout of the home page, such as the sidebars, the introduction as well as the date and time, as well as the div for the white box and a header which says "My account" - Account.css contains: The actual styling of the white box, such as the size of the box, the location of the box, the colour of  the box and a slight shadow to make the box stand out and look nicer.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="dashboard"),
+    path('', views.home, name="dashboard"),  # Home/Dashboard page
+    path('account/', views.account, name='account'),  # Account page
 ]
