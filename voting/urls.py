f976bc3068284ace6e83a576a6906405d35b944b
Nate Sinnott <ns@nathanielsinnott.com>
2025-04-23 17:02:29 +0100

    implemented js to display questions in order for survey. added  view to record submission to db. major changes to voting css to allow for accurate selection. todo: fix recording of health and trend attributes to surveyresponsedetail table.

--- voting/urls.py
+++ voting/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.survey, name='voting_survey'),  # main voting page
     path('newsurvey/', views.newsurvey, name='voting_newsurvey'),  # open new survey response page
+    path('submit/', views.voting_submit, name='voting_submit'),  # submit survey response
 ]
