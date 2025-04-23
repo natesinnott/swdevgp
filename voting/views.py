4c7532af1f2a80fccaa08677d3707c5273857165
Nate Sinnott <ns@nathanielsinnott.com>
2025-04-23 17:48:24 +0100

    removed unused import from views.py

--- voting/views.py
+++ voting/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils        import timezone
-from .models             import Session, Survey, SurveyResponse, SurveyDetail, SurveyResponseDetail, Employee
+from .models             import Session, SurveyResponse, SurveyDetail, SurveyResponseDetail, Employee
 
 # Create your views here.
 
