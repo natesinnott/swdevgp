ffc2878d13efbb7f4097ee0fff0e5263a6c17f98
Nate Sinnott <ns@nathanielsinnott.com>
2025-04-25 18:10:35 +0100

    - Created dashboard template and styling. Needs Django logic. - Updated dashboard.views - Updated Trends page with greeting and datetime

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from voting.models import Session, SurveyResponse, Employee
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render(request, "dashboard/dashboard.html")
+        # find an open session by datetime
+    now = timezone.now()
+    # Get the Employee record for the logged-in user
+    try:
+        employee = Employee.objects.get(user=request.user)
+    except Employee.DoesNotExist:
+        return redirect('dashboard')
+
+    sessions_qs = Session.objects.filter(
+        start_date__lte=now,
+        end_date__gte=now
+    )
+    has_active = sessions_qs.exists()
+    active_session = sessions_qs.first()
+    has_taken = False
+    if active_session:
+        has_taken = SurveyResponse.objects.filter(
+            employee=employee,
+            session=active_session
+        ).exists()
+
+    return render(request, 'dashboard/dashboard.html', {
+        'has_active_session': has_active,
+        'has_taken': has_taken,
+    })
 
 @login_required
 def account(request):
